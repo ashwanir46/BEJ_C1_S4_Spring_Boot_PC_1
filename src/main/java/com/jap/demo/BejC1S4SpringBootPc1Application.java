3916e63e557ce11545b247b7125f5d89126bd3be
Ashwani Rajput <ashwanir461@gmail.com>
2022-11-24 09:07:37 +0530

    done with all work

---
+++ src/main/java/com/jap/demo/BejC1S4SpringBootPc1Application.java
@@ -0,0 +1,15 @@
+package com.jap.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BejC1S4SpringBootPc1Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BejC1S4SpringBootPc1Application.class, args);
+        System.out.println("Good day to all");
+    }
+
+}
